b7b36faf3791378a7637f802d5b0fa4fa269fa6a
mperaic <mperaic@gmail.com>
2018-07-06 18:35:51 +0200

    5.4 authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
 
-before_filter
-ActionMailer::Base.default_url_options = {:host => request.host_with_port}
+before_action
+ActionMailer::Base.default_url_options = {:host => 'lotusartberlin.herokuapp.com'}
 end
