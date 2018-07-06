0d905443fa532e3820f6f3135d3196e06ee4ba56
mperaic <mperaic@gmail.com>
2018-07-06 19:46:07 +0200

    5.5 authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
 
-before_action
-ActionMailer::Base.default_url_options = {:host => 'lotusartberlin.herokuapp.com'}
+  before_action
+  ActionMailer::Base.default_url_options = {:host => 'lotusartberlin.herokuapp.com'}
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to main_app.root_url, alert: exception.message
+  end
 end
