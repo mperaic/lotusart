595bcabe4c97cdf56dbd41108cfd6315b5831729
mperaic <mperaic@gmail.com>
2018-07-06 13:33:20 +0200

    5.3 modify contact page sending emails

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :products
   get 'simple_pages/about'
   get 'simple_pages/contact'
+  post 'simple_pages/thank_you'
   get 'simple_pages/index'
   root 'simple_pages#landing_page'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
