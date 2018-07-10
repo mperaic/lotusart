96c2ec41fba2691bdaa321abeea84f1b45f83710
mperaic <mperaic@gmail.com>
2018-07-10 18:33:24 +0200

    5.2 forms

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,13 @@ class Product < ApplicationRecord
   has_many :comments
 
   def self.search(search_term)
-    Product.where("name LIKE ?", "%#{search_term}%")
+    if Rails.env.development?
+      # Dev Mode (sqlite3)
+      Product.where("name LIKE ?", "%#{search_term}%")
+    else
+      # Production or test mode (PostgreSQL)
+      Product.where("name ilike ?", "%#{search_term}%")
+    end
   end
 
   def highest_rating_comment
