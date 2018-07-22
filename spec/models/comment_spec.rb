f9f249d12488cc35cf1c5e493f4d30ac87b81507
mperaic <mperaic@gmail.com>
2018-07-22 15:16:13 +0200

    6.2 unit test

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,4 +1,12 @@
 require 'rails_helper'
 
 describe Comment do
-end
+  context "when the product has comments" do
+    let(:product) { Product.create!(name: "race bike") }
+    let(:user) { User.create!(email: "mperaic@gmail.com", password: "Perazhica")}
+
+  it "is not valid without a body" do
+    expect(Comment.new(body: " ")).not_to be_valid
+  end
+  end
+  end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
 
 describe Order do
+  
 end
