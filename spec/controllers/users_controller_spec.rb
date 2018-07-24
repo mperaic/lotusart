01817c6c9cb34b84e7a5180189fa486cc98096cb
mperaic <mperaic@gmail.com>
2018-07-24 19:50:04 +0200

    6.3 functional tests

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe UsersController, type: :controller do
+  let(:user) { User.create!(email: "mperaic@gmail.com", password: "Perazhica") }
+
+  describe 'GET #show' do
+    context 'when a user is logged in' do
+
+    before do
+      sign_in user
+    end
+
+    it "loads correct user details" do
+      expect(assigns(:user)).to eq user
+    end
+
+
+    end
+
+    end
+
+    context 'when a user is not logged in' do
+      it "redirects to login" do
+        get :show, params: { id: user.id }
+         expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+end
