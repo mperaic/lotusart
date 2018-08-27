05163269a7dcd10d4eb454a1a00194aa93bc37ef
mperaic <mperaic@gmail.com>
2018-08-27 15:02:50 +0200

    6.13 ActionCable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,4 +101,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.web_socket_server_url = "wss://lotusartberlin.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://lotusartberlin.herokuapp.com', 'http://lotusartberlin.herokuapp.com']
 end
