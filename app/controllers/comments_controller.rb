2fc54802fcb6f489b2df43c4ca9b929f8e1ce72e
mperaic <mperaic@gmail.com>
2018-09-03 13:32:11 +0200

    6.15 final

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,7 @@ class CommentsController < ApplicationController
     @comment = @product.comments.new(comment_params)
     @comment.user = current_user
     @user = current_user
-    
+
     respond_to do |format|
       if @comment.save
         ProductChannel.broadcast_to @product.id, comment: CommentsController.render(partial: 'comments/comment', locals: {comment: @comment, current_user: current_user}), average_rating: @product.average_rating
